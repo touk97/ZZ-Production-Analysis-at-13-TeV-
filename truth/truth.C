b8b95a0b5d9b97e527048552c4954141031357c8
Christos Touk <chtoukmen@gmail.com>
2023-08-04 11:46:49 +0300

    Script for truth level calculation in python

--- truth/truth.C
+++ truth/truth.C
@@ -27,6 +27,37 @@
 using namespace std;
 
 
+
+// CUSTOM STREAM BUFFER CLASS FOR OUTPUT LOG FILE
+class DualStreamBuffer : public std::streambuf
+{
+public:
+  // Constructor
+  DualStreamBuffer(std::streambuf *primaryBuffer, std::streambuf *secondaryBuffer)
+      : primaryBuffer(primaryBuffer), secondaryBuffer(secondaryBuffer) {}
+
+
+  int_type overflow(int_type c) override
+  {
+    if (c != EOF)
+    {
+      if (primaryBuffer != nullptr)
+        primaryBuffer->sputc(c);
+
+      if (secondaryBuffer != nullptr)
+        secondaryBuffer->sputc(c);
+    }
+    return c;
+  }
+
+private:
+  std::streambuf *primaryBuffer;  
+  std::streambuf *secondaryBuffer;
+};
+// END OF LOG FILE CLASS
+
+
+
 vector<Float_t> Counter(string category, string index1, string index2)
 {
 
@@ -57,7 +88,7 @@ vector<Float_t> Counter(string category, string index1, string index2)
   }
 
   cout << "    ENTRIES = " << tree->GetEntries() << endl << endl; 
-  cout << "          N = " << signal << "+-" << sqrt(signaler) << endl << endl; 
+  cout << "    N = " << signal << "+-" << sqrt(signaler) << endl << endl; 
 
 
   events.push_back(signal);
@@ -75,89 +106,101 @@ vector<Float_t> Counter(string category, string index1, string index2)
 void truth()
 {
 
-    Double_t weight;
-    Float_t signal_qq = 0.;
-    Float_t signal_qq_er = 0.;
-    Float_t signal_gg = 0.;
-    Float_t signal_gg_er = 0.;
-    Float_t signal_EWK = 0.;
-    Float_t signal_EWK_er = 0.;
+  // Output log file
+  ofstream logFile("./truthC.txt");
 
-    vector<Float_t> signal_qq1;
-    vector<Float_t> signal_qq2;
-    vector<Float_t> signal_qq3;
-    vector<Float_t> signal_gg1;
-    vector<Float_t> signal_gg2;
-    vector<Float_t> signal_gg3;
-    vector<Float_t> signal_EWK1;
-    vector<Float_t> signal_EWK2;
-    vector<Float_t> signal_EWK3;
+  DualStreamBuffer dualBuffer(std::cout.rdbuf(), logFile.rdbuf());
 
-    TH1::SetDefaultSumw2(kTRUE);
+  std::streambuf *oldBuffer = std::cout.rdbuf(&dualBuffer);
 
-    vector<string> categories = {"345666.Sherpa_222_NNPDF30NNLO_", "345723.Sherpa_222_NNPDF30NNLO_", "363724.MadGraphPythia8EvtGen_"};
-    vector<string> indexes1 = {"llvvZZ.deriv.DAOD_STDM3.e6240_s3126_", "ggllvvZZ.deriv.DAOD_STDM3.e6213_s3126_", "ZZllvv2jEWK.deriv.DAOD_STDM3.e5712_s3126_"};
-    vector<string> indexes2 = {"r9364_p4252.root.root", "r10201_p4252.root.root", "r10724_p4252.root.root"};
+  Double_t weight;
+  Float_t signal_qq = 0.;
+  Float_t signal_qq_er = 0.;
+  Float_t signal_gg = 0.;
+  Float_t signal_gg_er = 0.;
+  Float_t signal_EWK = 0.;
+  Float_t signal_EWK_er = 0.;
 
-    for (string &category : categories)
-    {
+  vector<Float_t> signal_qq1;
+  vector<Float_t> signal_qq2;
+  vector<Float_t> signal_qq3;
+  vector<Float_t> signal_gg1;
+  vector<Float_t> signal_gg2;
+  vector<Float_t> signal_gg3;
+  vector<Float_t> signal_EWK1;
+  vector<Float_t> signal_EWK2;
+  vector<Float_t> signal_EWK3;
+
+  TH1::SetDefaultSumw2(kTRUE);
+
+  vector<string> categories = {"345666.Sherpa_222_NNPDF30NNLO_", "345723.Sherpa_222_NNPDF30NNLO_", "363724.MadGraphPythia8EvtGen_"};
+  vector<string> indexes1 = {"llvvZZ.deriv.DAOD_STDM3.e6240_s3126_", "ggllvvZZ.deriv.DAOD_STDM3.e6213_s3126_", "ZZllvv2jEWK.deriv.DAOD_STDM3.e5712_s3126_"};
+  vector<string> indexes2 = {"r9364_p4252.root.root", "r10201_p4252.root.root", "r10724_p4252.root.root"};
+
+  for (string &category : categories)
+  {
       for (string &index1 : indexes1)
       {
+        
           for (string &index2 : indexes2)
           {
+            
               if (category == "345666.Sherpa_222_NNPDF30NNLO_" && index1 == "llvvZZ.deriv.DAOD_STDM3.e6240_s3126_")
               {
                   if (index2 == "r9364_p4252.root.root")
                   {
-                    cout << "   QCD qq1" << endl << endl;
+                    cout << "    QCD qq1" << endl << endl;
                     signal_qq1 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10201_p4252.root.root")
                   {
-                    cout << "   QCD qq2" << endl << endl;
+                    cout << "    QCD qq2" << endl << endl;
                     signal_qq2 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10724_p4252.root.root")
                   {
-                    cout << "   QCD qq3" << endl << endl;
+                    cout << "    QCD qq3" << endl << endl;
                     signal_qq3 = Counter(category, index1, index2);
                   }
+                  cout << "    --------------------------------------" << endl;
               }
               else if (category == "345723.Sherpa_222_NNPDF30NNLO_" && index1 == "ggllvvZZ.deriv.DAOD_STDM3.e6213_s3126_")
               {
                   if (index2 == "r9364_p4252.root.root")
                   {
-                    cout << "   QCD gg1" << endl << endl;
+                    cout << "    QCD gg1" << endl << endl;
                     signal_gg1 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10201_p4252.root.root")
                   {
-                    cout << "   QCD gg2" << endl << endl;
+                    cout << "    QCD gg2" << endl << endl;
                     signal_gg2 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10724_p4252.root.root")
                   {
-                    cout << "   QCD gg3" << endl << endl;
+                    cout << "    QCD gg3" << endl << endl;
                     signal_gg3 = Counter(category, index1, index2);
                   }
+                  cout << "    --------------------------------------" << endl;
               }
               else if (category == "363724.MadGraphPythia8EvtGen_" && index1 == "ZZllvv2jEWK.deriv.DAOD_STDM3.e5712_s3126_")
               {
                   if (index2 == "r9364_p4252.root.root")
                   {
-                    cout << "   QCD EWK2" << endl << endl;
+                    cout << "    QCD EWK2" << endl << endl;
                     signal_EWK1 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10201_p4252.root.root")
                   {
-                    cout << "   QCD EWK2" << endl << endl;
+                    cout << "    QCD EWK2" << endl << endl;
                     signal_EWK2 = Counter(category, index1, index2);
                   }
                   else if (index2 == "r10724_p4252.root.root")
                   {
-                    cout << "   QCD EWK3" << endl << endl;
+                    cout << "    QCD EWK3" << endl << endl;
                     signal_EWK3 = Counter(category, index1, index2);
                   }
+                  cout << "    --------------------------------------" << endl;
               }
           }
       }
@@ -166,11 +209,16 @@ void truth()
     signal_qq = signal_qq1[0] + signal_qq2[0] +  signal_qq3[0];
     signal_qq_er = signal_qq1[1] + signal_qq2[1] +  signal_qq3[1];
     signal_gg = signal_gg1[0] + signal_gg2[0] +  signal_gg3[0];
-    signal_gg_er = signal_gg1[1] + signal_gg2[1] +  signal_gg3[1];
-    signal_EWK = signal_EWK1[0] + signal_EWK2[0] +  signal_EWK3[0];
-    signal_EWK_er = signal_EWK1[1] + signal_EWK2[1] +  signal_EWK3[1];
+    signal_gg_er = signal_gg1[1] + signal_gg2[1] + signal_gg3[1];
+    signal_EWK = signal_EWK1[0] + signal_EWK2[0] + signal_EWK3[0];
+    signal_EWK_er = signal_EWK1[1] + signal_EWK2[1] + signal_EWK3[1];
 
-    cout << "Signal QCD qq:  " << signal_qq << " +- " << signal_qq_er << endl << endl;
-    cout << "Signal QCD gg:  " << signal_gg << " +- " << signal_gg_er << endl << endl;
-    cout << "Signal EWK:     " << signal_EWK << " +- " << signal_EWK_er << endl << endl;
+    printf("    --------------------------------------\n");
+    printf("    Signal QCD qq:  %.2f +- %.2f\n\n", signal_qq, signal_qq_er);
+    printf("    Signal QCD gg:  %.2f +- %.2f\n\n", signal_gg, signal_gg_er);
+    printf("    Signal EWK:     %.2f +- %.2f\n\n", signal_EWK, signal_EWK_er);
+
+    // For the log file
+    std::cout.rdbuf(oldBuffer); // Restore the original stream buffer for std::cout
+    logFile.close();            // Close the log file
 }
